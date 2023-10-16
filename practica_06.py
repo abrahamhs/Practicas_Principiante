4746069faa39545754181bdde503442071342b81
alex <abrahamhs1@gmail.com>
2023-10-16 11:26:54 +0200

    Display numbers divisible by 5 from a list

---
+++ practica_06.py
@@ -0,0 +1,7 @@
+# Iterate the given list of numbers and print only those numbers which are divisible by 5
+lista1 = [10, 20, 33, 46, 55]
+print(lista1)
+for num in lista1:
+  if num % 5 == 0:
+    print(num)
+    
