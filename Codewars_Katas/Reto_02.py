ee07ff2b7f4b620a471963713f0b9935fbfecdec
abraham-dev <abrahamhs1@gmail.com>
2023-10-20 20:54:10 +0200

    Solucion con comentarios

--- Codewars_Katas/Reto_02.py
+++ Codewars_Katas/Reto_02.py
@@ -9,3 +9,45 @@ Ejemplos:
 
 [160, 3, 1719, 19, 11, 13, -21] --> 160 (the only even number)
 """
+def find_outlier(matriz):
+    # variable de control
+    par = 0
+    impar = 0
+    cont = 0
+    n = []
+    # verifico si el array tiene mas de 3 elementos para proceder a verificarlo e caso contrario retorno None
+    if len(matriz) < 3:
+        return None
+    else:
+        # aqui compruebo si es un array mayormente impar o par
+        for i in range(len(matriz)):
+            if matriz[i] % 2 == 0:
+                par += 1   # sumo los nuemros pares del array
+            else:
+                impar += 1 # sumo los numeros impares del array
+    # valido si voy a trabajar un array par
+    if par > impar:
+        for j in range(len(matriz)): # recorro la matriz
+            if matriz[j] % 2 != 0:  # valido cual es ese numero que no es par
+                n = matriz[j] # procedo a guardarlo
+                cont +=1  # contador de control para verificar que solo se dara el resultado si hay un solo numero impar
+        if cont == 1: # si al final de recorrer la matriz el contador es igual a 1 retorno n que tiene el valor de ese N impar
+            return print(f'{n} el unico numero par')
+        else:
+            return None   # si el contador supera 1 retorno None
+    # si no es un array para es impar    
+    else:
+        for j in range(len(matriz)):  # igual que el procesado de los pares pero para matrices impares
+            if matriz[j] % 2 != 1:
+                n = matriz[j]
+                cont += 1
+        if cont == 1:
+            return print(f'{n} el unico numero impar')
+        else:
+            return None
+       
+# Test
+buscar_atipico = [2, 4, 0, 100, 4, 11, 2602, 36] 
+find_outlier(buscar_atipico)
+buscar_atipico = [160, 3, 1719, 19, 11, 13, -21]
+find_outlier(buscar_atipico)      
