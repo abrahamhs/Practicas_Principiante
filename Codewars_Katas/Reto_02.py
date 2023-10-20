ac6d28259df7b300f94826eec1f62f0c11e7a54a
abraham-dev <abrahamhs1@gmail.com>
2023-10-20 16:12:48 +0200

    Kata 02 Encontrar el valor atípico de la paridad

---
+++ Codewars_Katas/Reto_02.py
@@ -0,0 +1,11 @@
+""" Se le da una matriz (que tendrá una longitud de al menos 3, 
+pero podría ser muy grande) que contiene números enteros. 
+La matriz estára toda compuesta  por enteros impares o  por enteros pares
+con excepcion por un único entero N en la matriz de los pares este sera impar
+en la matriz de los pares este sera par. 
+Escriba un método que tome la matriz como argumento y devuelva este "valor atípico" de N.
+Ejemplos:
+[2, 4, 0, 100, 4, 11, 2602, 36] -->  11 (the only odd number)
+
+[160, 3, 1719, 19, 11, 13, -21] --> 160 (the only even number)
+"""
