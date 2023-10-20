2339853e2185750879c40c4ecc720cdb22a9e28e
abraham-dev <abrahamhs1@gmail.com>
2023-10-20 19:27:11 +0200

    erratas en la kata

--- Codewars_Katas/Reto_02.py
+++ Codewars_Katas/Reto_02.py
@@ -2,7 +2,7 @@
 pero podría ser muy grande) que contiene números enteros. 
 La matriz estára toda compuesta  por enteros impares o  por enteros pares
 con excepcion por un único entero N en la matriz de los pares este sera impar
-en la matriz de los pares este sera par. 
+en la matriz de los impares este sera par. 
 Escriba un método que tome la matriz como argumento y devuelva este "valor atípico" de N.
 Ejemplos:
 [2, 4, 0, 100, 4, 11, 2602, 36] -->  11 (the only odd number)
