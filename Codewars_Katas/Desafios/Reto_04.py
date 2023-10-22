bf058f94fcc21e923977caa2d18e935c5d4a3181
abraham-dev <abrahamhs1@gmail.com>
2023-10-22 11:18:22 +0200

    Convert string to camel case  nivel < 6kyu >

---
+++ Codewars_Katas/Desafios/Reto_04.py
@@ -0,0 +1,24 @@
+# Convert string to camel case  nivel < 6kyu >
+"""
+Complete the method/function so that it converts dash/underscore 
+delimited words into camel casing. The first word within the output 
+should be capitalized only if the original word was capitalized 
+(known as Upper Camel Case, also often referred to as Pascal case). 
+The next words should be always capitalized.
+
+Complete el método/función para que convierta las palabras 
+delimitadas por guiones/guiones bajos en mayúsculas y minúsculas. 
+La primera palabra de la salida debe ir en mayúsculas sólo si la 
+palabra original iba en mayúsculas (lo que se conoce como 
+Mayúsculas Camel Case, también conocido como Pascal case). Las palabras 
+siguientes deben ir siempre en mayuscula
+
+Ejemplos:
+
+"the-stealth-warrior" --->  "theStealthWarrior"
+
+"The_Stealth_Warrior" ---> "TheStealthWarrior"
+
+"The_Stealth-Warrior" ---> "TheStealthWarrior"
+
+"""
