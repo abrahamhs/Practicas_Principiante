76c04cc023579dd680314a229410b340dfc7ecd3
abraham-dev <abrahamhs1@gmail.com>
2023-10-23 07:30:45 +0200

    Split Strings  nivel  < 6kyu >

---
+++ Codewars_Katas/Desafios/Retos_05.py
@@ -0,0 +1,20 @@
+# Split Strings  nivel < 6kyu >
+
+"""
+Complete the solution so that it splits the string into pairs of two characters. 
+If the string contains an odd number of characters then it should replace the missing 
+second character of the final pair with an underscore ('_').
+
+Complete la solución para que divida la cadena en pares de dos caracteres. 
+Si la cadena contiene un número impar de caracteres, debe sustituir el segundo carácter 
+que falta del último par por un guión bajo ('_').
+
+Examples:
+
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef']
+
+"""
+
+def solution(s):
+    pass
